202e08358df91d112c9071e8234c741bdc6ab202
tanisha-keshri <tanishapcm@gmail.com>
2019-05-31 17:42:30 +0530

    Vue demo added

---
+++ Demo_vue/index.js
@@ -0,0 +1,32 @@
+new Vue({ //binds the view and the model(data)
+    el: '#app', // attaches with the dom with ID app 
+    data: {     // all the data is defined in this secction
+        message: 'Hello Vue.js!',
+        tools: [
+          {text: "javaScript"},
+          {text: "typeScript"},
+          {text: "webpack"},
+          {text: "Angular"},
+          {text: "How could you forget Vue :/"}
+        ],
+        newTool: "",
+        flag: true
+    },
+    methods: {
+    greet: function (event) {
+      this.message += "boom";
+    },
+    say: function (msg){
+    	this.message = msg;
+    },
+    addTool: function (){
+    	text = this.newTool.trim();
+      if(text){
+      this.tools.push({text: text});
+      this.newTool = "";
+      }
+    }
+    }
+})
+
+// build large-scale applications composed of small, self-contained, and often reusable components
